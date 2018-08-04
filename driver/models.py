5d181c268d16ffe45b6358287e56e24cb38c2d75
kimani-njoroge <kimani064@gmail.com>
2018-08-04 22:02:22 +0300

    create car model

--- driver/models.py
+++ driver/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    car_brand = models.CharField(max_length=20)
+    no_plate = models.CharField(max_length=25)
+    no_seat = models.IntegerField(max_length=20)
+
+
