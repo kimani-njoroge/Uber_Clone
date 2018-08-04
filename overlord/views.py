8cee151eb80c81341e2e12605ad7ca5f5104b9f1
kimani-njoroge <kimani064@gmail.com>
2018-08-04 22:46:00 +0300

    create driver signup view

--- overlord/views.py
+++ overlord/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from .forms import DriverSignupForm
 
+User = get_user_model()
 # Create your views here.
+def driver_signup(request):
+    if request.method == 'POST':
+        form = DriverSignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = True
+            user.is_passenger = False
+            user.is_driver = True
+            user.save()
+            return redirect('/')
+        else:
+            form = DriverSignupForm()
+    return render(request,'registration/driver_signup.html',{"form":form})
