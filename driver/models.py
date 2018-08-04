ee77bb0a0eff6761ee6cfaf938c6c3a74b4b52cf
kimani-njoroge <kimani064@gmail.com>
2018-08-04 22:10:28 +0300

    create location model

--- driver/models.py
+++ driver/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Car(models.Model):
     car_brand = models.CharField(max_length=20)
     no_plate = models.CharField(max_length=25)
-    no_seat = models.IntegerField(max_length=20)
+    no_seat = models.IntegerField(default=0)
 
 
 class Driver(models.Model):
--- rider/models.py
+++ rider/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=40)
+    longitude = models.IntegerField(default=0)
+    latitude = models.IntegerField(default=0)
