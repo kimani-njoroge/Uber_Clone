85081e2cab9144aac26560aa25c6ad8442d4007a
kimani-njoroge <kimani064@gmail.com>
2018-08-04 22:14:02 +0300

    create passenger model

--- rider/models.py
+++ rider/models.py
@@ -5,3 +5,10 @@ class Location(models.Model):
     name = models.CharField(max_length=40)
     longitude = models.IntegerField(default=0)
     latitude = models.IntegerField(default=0)
+
+
+class Passenger(models.Model):
+    pass_name = models.CharField(max_length=40)
+    location = models.ForeignKey(Location)
+    pass_pick = models.CharField(max_length=40)
+    id = models.IntegerField(default=0)
