bdd57f4bb13b9bb4165217947beee3e217886bc4
kimani-njoroge <kimani064@gmail.com>
2018-08-05 00:57:50 +0300

    add logout url

--- lyft/urls.py
+++ lyft/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('overlord.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
