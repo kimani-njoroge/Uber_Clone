94683314f37b580a42c028b44f1f51d8c7845695
kimani-njoroge <kimani064@gmail.com>
2018-08-06 09:55:24 +0300

    create method to save driver instance

--- driver/models.py
+++ driver/models.py
@@ -4,11 +4,13 @@ from django.db import models
 # Create your models here.
 
 
-
 class Car(models.Model):
     car_brand = models.CharField(max_length=20)
     no_plate = models.CharField(max_length=25)
     no_seat = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.car_brand
 
 
 class Driver(models.Model):
@@ -17,3 +19,6 @@ class Driver(models.Model):
     pickup_point = models.CharField(max_length=30)
     destination = models.CharField(max_length=30)
     user = models.ForeignKey(User,blank=True)
+
+    def passprofile_save(self):
+        self.save()
